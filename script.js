078aae7b71082e307da87fc780318b4e09ea6fef
Fiona Morris <morri22f@gmail.com>
2021-03-25 14:03:31 -0400

    Script Update
    Implemented solution for email validation and PDF download.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function download(str) {
-    console.log(str);
+var button = document.querySelectorAll("button");
+button.disabled = true;
 
+
+function validateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(form.email.value)) {
+        button.disabled = false;
+
+    } else {
+        alert("Please enter a valid email address. Thank you!");
+    }
+}
+
+function download(str) {
+    if (button.disabled == 0) {
+        window.open(str);
+    }
 }
