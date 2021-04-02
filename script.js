bcb50bcc1ca8b93f1bc1fdbc52f05c5656fe3290
Fiona Morris <morri22f@gmail.com>
2021-04-02 13:00:57 -0400

    Site Updates
    Updates made to the library, gallery, and contact page, following second content and design meeting.

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbx6w2H2B8IafDlNvKhTJKONOH-cOQf61GpiD_ACqDL9bmvr2lCwVhSljrYbBKW990v3HQ/exec'
-
-$('#submit-form').on('click', function(e) {
-    e.preventDefault();
-    var jqxhr = $.ajax({
-        url: url,
-        method: "GET",
-        dataType: "json",
-        data: $form.serializeObject()
-    }).success(
-        // do something
-    );
-})
+function download(str) {
+    window.open(str);
+    return;
+}
